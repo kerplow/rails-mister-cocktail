3fb642f8cfbdb8d38d87387cc82340490a4952f3
Charles de Gheldere <charlesdegh@gmail.com>
2017-11-10 17:36:49 +0100

    added reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
 
